225b0a45e0e79472c5f668874f1da5ded20a0055
overheat <aaron.tsui@outlook.com>
2022-03-15 09:06:57 +0800

    lambda-runtime/examples/basic.rs

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,26 +1,53 @@
-use lambda_runtime::{handler_fn, Context, Error};
-use log::LevelFilter;
-use serde_json::{json, Value};
-use simple_logger::SimpleLogger;
+// This example requires the following input to succeed:
+// { "command": "do something" }
+
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde::{Deserialize, Serialize};
+
+/// This is also a made-up example. Requests come into the runtime as unicode
+/// strings in json format, which can map to any structure that implements `serde::Deserialize`
+/// The runtime pays no attention to the contents of the request payload.
+#[derive(Deserialize)]
+struct Request {
+    command: String,
+}
+
+/// This is a made-up example of what a response structure may look like.
+/// There is no restriction on what it can be. The runtime requires responses
+/// to be serialized into json. The runtime pays no attention
+/// to the contents of the response payload.
+#[derive(Serialize)]
+struct Response {
+    req_id: String,
+    msg: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-  SimpleLogger::new()
-    .with_level(LevelFilter::Info)
-    .init()
-    .unwrap();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // this needs to be set to false, otherwise ANSI color codes will
+        // show up in a confusing manner in CloudWatch logs.
+        .with_ansi(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
 
-  let func = handler_fn(handler);
-  lambda_runtime::run(func).await?;
-  Ok(())
+    let func = service_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
 }
 
-async fn handler(event: Value, _: Context) -> Result<Value, Error> {
-  let message = event["message"].as_str().unwrap_or("world");
-  let first_name = event["firstName"].as_str().unwrap_or("Anonymous");
+pub(crate) async fn my_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
+    // extract some useful info from the request
+    let command = event.payload.command;
 
-  let response = format!("Hello, {}! Your name is {}", message, first_name);
-  log::info!("{}", response);
+    // prepare the response
+    let resp = Response {
+        req_id: event.context.request_id,
+        msg: format!("Command {} executed.", command),
+    };
 
-  Ok(json!({ "response": response }))
+    // return `Response` (it will be serialized to JSON automatically by the runtime)
+    Ok(resp)
 }
